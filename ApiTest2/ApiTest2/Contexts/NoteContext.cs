8e38d78ec3b42651b07fbb49db069aa09e57d53a
tixdim <tixdim.on1@gmail.com>
2021-10-02 16:49:19 +0300

    обнова в бравле 15 минут

---
+++ ApiTest2/ApiTest2/Contexts/NoteContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiTest2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTest2.Contexts
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> option) : base(option) { }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ ApiTest2/ApiTest2/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTest2.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        // prop + tab + tab
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? ChangeDate { get; set; }
+    }
+}
